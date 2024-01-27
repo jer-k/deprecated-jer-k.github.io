f1898aa0cba9c5982e53c0d5fc647d99c2d086d9
jer-k <jeremyk112@gmail.com>
2024-01-27 09:32:28 -0800

    Attempt to a redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,3 +2,13 @@
 
 exports.createPages = require('./gatsby/create-pages');
 exports.onCreateNode = require('./gatsby/on-create-node');
+
+exports.createPages = async ({ actions }) => {
+  const { createRedirect } = actions
+
+  createRedirect({
+    fromPath: `/getting-started`,
+    toPath: `https://jeremykreutzbender.com/blog/getting-started`,
+    statusCode: 200,
+  })
+}
